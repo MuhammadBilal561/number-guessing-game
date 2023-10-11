7347bed15a65efadcf12c87d15dd7ea5b87cd40c
MuhammadBilal561 <bilalrehan2006@gmail.com>
2023-10-11 11:36:38 -0700

    first commit

---
+++ node_modules/guess-game-mb/index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+
+type answerType={
+    userGuess:number
+};
+
+const systemGeneratedNo:number=Math.floor(Math.random() *100);
+
+
+const answers:answerType= await inquirer
+  .prompt([
+   {
+    type:"number",
+    name:"userGuess",
+    message:"Guess a number between 0 and 100?"
+   }
+  ]);
+  
+const {userGuess} =answers;
+
+if(userGuess===systemGeneratedNo){
+
+console.log("You Guess a right number!")
+}
+else if (userGuess <0 || userGuess>100){
+console.log("please enter a number between 0 & 100")
+}
+
+else{
+    console.log("You Guess a wrong number!")
+}
+console.log("your guess:" , userGuess , "&", "Correct answer:",systemGeneratedNo)
